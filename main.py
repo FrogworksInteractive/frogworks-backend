f64fbe80004e9c72317e8fd0b1c7122c0fb678b4
DJ <danieldcookjr@gmail.com>
2024-09-12 16:53:25 -0700

    Added file-related constants and initialization. Fixed a bug with the missing parameters response

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 
 from dotenv import load_dotenv
+from datetime import datetime
 from datetime import date
 
 # Flask-related imports.
@@ -11,12 +12,21 @@ from flask_restful import Api
 from database import Database
 from email_manager import EmailManager
 from api_resource import APIResource
+from file_manager import FileManager
 from structures.user import User
 from utils import Utils
+
+
+# Constants.
+BASE_DIRECTORY: str = 'data'
+PHOTOS_DIRECTORY: str = 'photos'
+APPLICATIONS_DIRECTORY: str = 'applications'
+
 
 # Variables.
 email_manager: EmailManager | None = None
 database: Database | None = None
+file_manager: FileManager | None = None
 app = None
 api = None
 
@@ -36,7 +46,7 @@ class RequestEmailVerification(APIResource):
         missing, parameters = self.missing_parameters()
 
         if missing:
-            return {'missing_parameters': missing}, 400
+            return {'missing_parameters': parameters}, 400
 
         # Get the email address from the form data.
         email_address: str = request.form.get('email_address')
@@ -55,7 +65,7 @@ class CheckEmailVerification(APIResource):
         missing, parameters = self.missing_parameters()
 
         if missing:
-            return {'missing_parameters': missing}, 400
+            return {'missing_parameters': parameters}, 400
 
         # Get the parameters.
         email_address: str = request.form.get('email_address')
@@ -75,7 +85,7 @@ class Register(APIResource):
         missing, parameters = self.missing_parameters()
 
         if missing:
-            return {'missing_parameters': missing}, 400
+            return {'missing_parameters': parameters}, 400
 
         # Get the parameters.
         username: str = request.form.get('username')
@@ -101,7 +111,7 @@ class Login(APIResource):
         missing, parameters = self.missing_parameters()
 
         if missing:
-            return {'missing_parameters': missing}, 400
+            return {'missing_parameters': parameters}, 400
 
         # Get the parameters.
         username: str = request.form.get('username')
@@ -136,6 +146,45 @@ class Login(APIResource):
         session_id: str = response['session_id']
 
         return {'session_id': session_id}, 200
+
+
+class GetUser(APIResource):
+    required_parameters = ['user_id']
+
+    def get(self):
+        missing, parameters = self.missing_parameters()
+
+        if missing:
+            return {'missing_parameters': parameters}, 400
+
+        # Ensure that the user is authenticated.
+        authenticated, session_id = self.get_authentication()
+
+        if not authenticated:
+            return {}, 401
+
+        session = database.get_session(session_id)
+
+        if not session:
+            return {}, 403
+
+        # Get the user.
+        session_user = database.get_user(session.user_id)
+
+        # Ensure that the user has developer permissions.
+        if not session_user:
+            return {'details': 'The session\'s user does not exist.'}, 400
+
+        # Get the parameters.
+        user_id: int = Utils.safe_int_cast(request.form.get('user_id'))
+
+        # Get the requested user.
+        user = database.get_user(user_id)
+
+        if not user:
+            return {'details': 'The specified user does not exist.'}, 400
+
+        return user.into_dict(user.is_or_admin(session_user.id)), 200
 
 
 class AuthenticateSession(APIResource):
@@ -179,9 +228,210 @@ class DeleteSession(APIResource):
         return {}
 
 
+class CreateApplication(APIResource):
+    required_parameters = ['name', 'package_name', 'type', 'description', 'release_date', 'early_access',
+                           'supported_platforms', 'genres', 'tags', 'base_price']
+
+    def post(self):
+        missing, parameters = self.missing_parameters()
+
+        if missing:
+            return {'missing_parameters': parameters}, 400
+
+        # Ensure that the user is authenticated.
+        authenticated, session_id = self.get_authentication()
+
+        if not authenticated:
+            return {}, 401
+
+        session = database.get_session(session_id)
+
+        if not session:
+            return {}, 403
+
+        # Get the user.
+        user = database.get_user(session.user_id)
+
+        # Ensure that the user has developer permissions.
+        if not user:
+            return {'details': 'The session\'s user does not exist.'}, 400
+
+        if not user.has_developer_permissions():
+            return {'details': 'You are not a developer!'}, 403
+
+        # Get the parameters.
+        name: str = request.form.get('name')
+        package_name: str = request.form.get('package_name')
+        type_: str = request.form.get('type')
+        description: str = request.form.get('description')
+        release_date: date = datetime.strptime(request.form.get('release_date'), '%Y-%m-%d').date()
+        early_access: bool = Utils.safe_bool_cast(request.form.get('early_access'))
+        latest_version: str = '' # Empty, signifying that there is no version available (at this time).
+        supported_platforms: list = request.form.get('supported_platforms').split(',')
+        genres: list = request.form.get('genres').split(',')
+        tags: list = request.form.get('tags').split(',')
+        base_price: float = Utils.safe_float_cast(request.form.get('base_price'))
+        owners: list = [str(user.id)]
+
+        # Attempt to create the application.
+        success, response = database.create_application(
+            name,
+            package_name,
+            type_,
+            description,
+            release_date,
+            early_access,
+            latest_version,
+            supported_platforms,
+            genres,
+            tags,
+            base_price,
+            owners
+        )
+
+        # Create the application's data folder (for storing versions).
+        file_manager.create_application_folder(package_name)
+
+        if not success:
+            return response, 400
+
+        return {'details': 'Successfully created application.', 'application_id': response['application_id']}, 200
+
+
+class UpdateApplicationVersion(APIResource):
+    required_parameters = ['application_id', 'version']
+
+    def put(self):
+        missing, parameters = self.missing_parameters()
+
+        if missing:
+            return {'missing_parameters': parameters}, 400
+
+        # Ensure that the user is authenticated.
+        authenticated, session_id = self.get_authentication()
+
+        if not authenticated:
+            return {}, 401
+
+        session = database.get_session(session_id)
+
+        if not session:
+            return {}, 403
+
+        # Get the user.
+        user = database.get_user(session.user_id)
+
+        # Ensure that the user has developer permissions.
+        if not user:
+            return {'details': 'The session\'s user does not exist.'}, 400
+
+        if not user.has_developer_permissions():
+            return {'details': 'You are not a developer!'}, 403
+
+        # Get the parameters.
+        application_id: int = Utils.safe_int_cast(request.form.get('application_id'))
+        version: str = request.form.get('version')
+
+        # Ensure that the application exists.
+        # Get the application.
+        application = database.get_application(application_id)
+
+        if not application:
+            return {'details': 'The specified application does not exist.'}, 400
+
+        # Ensure that the user is an owner of this application.
+        if not user.id in application.owners:
+            return {'details': 'This is not your application; you cannot update its version.'}, 403
+
+        database.update_application_property(application_id, 'latest_version', version)
+
+        return {}
+
+
+class CreateVersion(APIResource):
+    required_parameters = ['application_id', 'name', 'platform', 'release_date', 'filename', 'executable']
+    required_files = ['file']
+
+    def post(self):
+        missing, parameters = self.missing_parameters()
+
+        if missing:
+            return {'missing_parameters': parameters}, 400
+
+        # Ensure that the user is authenticated.
+        authenticated, session_id = self.get_authentication()
+
+        if not authenticated:
+            return {}, 401
+
+        session = database.get_session(session_id)
+
+        if not session:
+            return {}, 403
+
+        # Get the user.
+        user = database.get_user(session.user_id)
+
+        # Ensure that the user has developer permissions.
+        if not user:
+            return {'details': 'The session\'s user does not exist.'}, 400
+
+        if not user.has_developer_permissions():
+            return {'details': 'You are not a developer!'}, 403
+
+        # Get the parameters.
+        application_id: int = Utils.safe_int_cast(request.form.get('application_id'))
+        name: str = request.form.get('name')
+        platform: str = request.form.get('platform')
+        release_date: date = datetime.strptime(request.form.get('release_date'), '%Y-%m-%d').date()
+        filename: str = Utils.generate_uuid4() + '_' + request.form.get('filename')
+        executable = request.form.get('executable')
+
+        # Ensure that the application exists.
+        # Get the application.
+        application = database.get_application(application_id)
+
+        if not application:
+            return {'details': 'The specified application does not exist.'}, 400
+
+        # Ensure that the user is an owner of this application.
+        if not user.id in application.owners:
+            return {'details': 'This is not your application; you cannot push versions to it.'}, 403
+
+        # Everything is good; create the version.
+        # Ensure that a file was provided.
+        missing_files, missing_files_list = self.missing_files()
+
+        if missing_files:
+            return {'details': 'Please provide a version file.'}, 400
+
+        # Create the version entry in the database first to make sure there is not an issue.
+        success, response = database.create_application_version(
+            application_id,
+            name,
+            platform,
+            release_date,
+            filename,
+            executable
+        )
+
+        # Handle errors.
+        if not success:
+            return response, 400
+
+        # No issues creating the version; upload the file.
+        # Get the version file.
+        version_file = request.files['file']
+
+        # Save the version file.
+        version_file.save(file_manager.generate_version_filepath(application.package_name, filename))
+
+        return response, 200
+
+
 # Main method.
 def main():
-    global email_manager, database, app, api
+    global email_manager, database, file_manager, app, api
 
     # Load the .env environment variables.
     load_dotenv()
@@ -192,6 +442,10 @@ def main():
     # Initialize the database.
     database = Database(email_manager)
     database.initialize()
+
+    # Initialize the file manager.
+    file_manager = FileManager(database, BASE_DIRECTORY, PHOTOS_DIRECTORY, APPLICATIONS_DIRECTORY)
+    file_manager.initialize()
 
     # Load the HTTP server port.
     server_port: int = int(os.getenv('SERVER_PORT'))
@@ -206,8 +460,12 @@ def main():
     api.add_resource(CheckEmailVerification, '/api/email-verification/check')
     api.add_resource(Register, '/api/user/register')
     api.add_resource(Login, '/api/user/login')
+    api.add_resource(GetUser, '/api/user/get')
     api.add_resource(AuthenticateSession, '/api/session/authenticate')
     api.add_resource(DeleteSession, '/api/session/delete')
+    api.add_resource(CreateApplication, '/api/application/create')
+    api.add_resource(UpdateApplicationVersion, '/api/application/update-version')
+    api.add_resource(CreateVersion, '/api/version/create')
 
     # Run the HTTP server.
     app.run(
