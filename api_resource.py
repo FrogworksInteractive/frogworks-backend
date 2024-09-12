419bafb7170440565cc68c4975c1e26cc0072f6e
DJ <danieldcookjr@gmail.com>
2024-09-12 16:51:39 -0700

    Added helper method(s)

--- api_resource.py
+++ api_resource.py
@@ -4,11 +4,17 @@ from flask_restful import Resource
 
 class APIResource(Resource):
     required_parameters: list = []
+    required_files: list = []
 
     def missing_parameters(self) -> tuple[bool, list]:
         parameters = [parameter for parameter in self.required_parameters if parameter not in request.form]
 
         return len(parameters) > 0, parameters
+
+    def missing_files(self) -> tuple[bool, list]:
+        files = [file for file in self.required_files if file not in request.files]
+
+        return len(files) > 0, files
 
     @staticmethod
     def get_authentication() -> tuple[bool, str | None]:
