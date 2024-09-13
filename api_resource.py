4c36355639daa3570bad8d80f222bbabcaaba96c
DJ <danieldcookjr@gmail.com>
2024-09-12 17:26:21 -0700

    Added helper method

--- api_resource.py
+++ api_resource.py
@@ -1,6 +1,10 @@
 from flask import request
 from flask_restful import Resource
 
+from database import Database
+from structures.session import Session
+from structures.user import User
+
 
 class APIResource(Resource):
     required_parameters: list = []
@@ -15,6 +19,32 @@ class APIResource(Resource):
         files = [file for file in self.required_files if file not in request.files]
 
         return len(files) > 0, files
+
+    def verify_session(self, database: Database) -> tuple[bool, dict, int, Session | None, User | None]:
+        missing, parameters = self.missing_parameters()
+
+        if missing:
+            return False, {'missing_parameters': parameters}, 400, None, None
+
+        # Ensure that the user is authenticated.
+        authenticated, session_id = self.get_authentication()
+
+        if not authenticated:
+            return False, {}, 401, None, None
+
+        session = database.get_session(session_id)
+
+        if not session:
+            return False, {}, 403, None, None
+
+        # Get the user.
+        user = database.get_user(session.user_id)
+
+        # Ensure that the user exists.
+        if not user:
+            return False, {'details': 'The session\'s user does not exist.'}, 400, None, None
+
+        return True, {}, -1, session, user
 
     @staticmethod
     def get_authentication() -> tuple[bool, str | None]:
